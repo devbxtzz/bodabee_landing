c8f6eae583b9c77410df066b770f45aad580e0e3
devbxtzz <batztonnie@gmail.com>
2023-07-11 09:38:36 +0300

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import screen1 from "../assets/screen1.png";
 
 function About() {
   return (
@@ -10,8 +9,9 @@ function About() {
       <h2 className="py-12 text-txtClr">About us</h2>
 
       <div className="layout-margin-tight flex flex-col justify-center items-center gap-12 py-12 text-left">
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center gap-4">
-          <div className="w-[95%] sm:w-1/2">
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center">
+          <div className="">
+            <h4 className="py-12 text-black">Redefining the Way You Cycle</h4>
             <p className="mt-4 text-xl">
               At EcstasyBikes, we are passionate about revolutionizing the way
               people experience and embrace cycling. We believe that bicycles
@@ -21,10 +21,41 @@ function About() {
               inspiring them to embrace the joy of cycling and unlock the
               countless benefits it offers.
             </p>
-          </div>
+            <h4 className="py-12 text-black">
+              Pedaling Towards a Greener Future
+            </h4>
+            <p className="mt-4 text-xl">
+              As avid cyclists, we are committed to promoting sustainability and
+              reducing our environmental impact. By choosing to cycle instead of
+              driving, you contribute to a cleaner, greener planet. We strive to
+              create a culture of eco-consciousness by encouraging individuals
+              to adopt cycling as a sustainable mode of transportation.
+              Together, we can make a positive difference for future
+              generations.
+            </p>
 
-          <div className="relative w-1/2 flex justify-center items-center">
-            <img src={screen1} alt="/" className="absolute -z-10" />
+            <h4 className="py-12 text-black">
+              Empowering Riders, Connecting Communities
+            </h4>
+            <p className="mt-4 text-xl">
+              Our mission is simple: to empower riders and connect communities
+              through the love of cycling. We strive to make cycling accessible
+              and enjoyable for everyone, whether you're a seasoned cyclist or a
+              novice enthusiast. By providing a wide range of bicycles, flexible
+              rental options, and a user-friendly platform, we aim to remove
+              barriers and make cycling a part of your everyday life.
+            </p>
+
+            <h4 className="py-12 text-black">Cycling is Better Together</h4>
+            <p className="mt-4 text-xl">
+              We believe that cycling is more than just a solitary activity;
+              it's an opportunity to connect with fellow riders and build a
+              thriving community. EcstasyBikes is a place where cyclists of all
+              levels can come together, share experiences, and inspire one
+              another. Join us in exploring new routes, participating in events,
+              and fostering a supportive environment that encourages growth and
+              camaraderie.
+            </p>
           </div>
         </div>
 
